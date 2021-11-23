cdbe91d017a18784cc3e4591a0bbc14408aba26d
dipti <dipti.yadav@outlook.com>
2021-11-23 13:06:21 -0500

    Day of the programmer

--- dayOfTheProgrammer.js
+++ dayOfTheProgrammer.js
@@ -1,6 +1,10 @@
 function dayOfProgrammer(year) {
   if (year == 1918) return "26.09.1918";
   else if (
+    // we are checking year is leep year or not
+    // In the question  it is given that the leap year;
+    //Divisible by 400.
+    // Divisible by 4 and not divisible by 100.
     (year <= 1917 && year % 4 == 0) ||
     year % 400 == 0 ||
     (year % 4 == 0) & (year % 100 != 0)

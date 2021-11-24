08dbc3cec5361086c564b04254ec5ebe2595e980
dipti <dipti.yadav@outlook.com>
2021-11-24 17:09:51 -0500

    Drawing Book

--- drawingBook.js
+++ drawingBook.js
@@ -1,4 +1,9 @@
 function pageCount(n, p) {
+  //GIVEN
+  //When they open the book,
+  //page  is always on the right side.
+  //When they flip page 1 , they see pages 2 and3 ;
+  //Each page except the last page will always be printed on both sides.
   let frontFlip = Math.floor(p / 2);
   let backFlip = Math.floor(n / 2 - frontFlip);
 

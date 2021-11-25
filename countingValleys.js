43c74ad83a7c5c097f8faef196be7112d3b10f11
dipti <dipti.yadav@outlook.com>
2021-11-25 18:56:38 -0500

    Counting Valleys

--- countingValleys.js
+++ countingValleys.js
@@ -1,3 +1,11 @@
+/**  => A mountain is a sequence of consecutive steps above sea level, 
+starting with a step up from sea level and 
+ending with a step down to sea level. 
+=> A mountain is a sequence of consecutive steps above sea level,
+ starting with a step up from sea level and 
+ ending with a step down to sea level.
+*/
+
 function countingValleys(steps, path) {
   let count = 0;
   let height = 0;
